ab6ce3350063b3805a1339b2f677f0bd972fa68a
Lusine-Sargs-yan <sargsyanlq@gmail.com>
2023-06-05 19:43:21 +0400

    add quick sort

---
+++ quick-sort/README.md
@@ -0,0 +1,2 @@
+## Quick sort
+### Given an array of n elements. Return sorted array.
---
+++ quick-sort/index.js
@@ -0,0 +1,21 @@
+const quickSort = arr => {
+    if(arr.length < 2) return arr;
+
+    const pivot = arr[arr.length -1];
+    const right = [];
+    const left = [];
+    for(let i = 0; i < arr.length - 1; i++) {
+        const item = arr[i];
+        if(item <= pivot) {
+            left.push(item);
+        } else {
+            right.push(item);
+        }
+    }
+
+    return [...quickSort(left), pivot, ...quickSort(right)];
+
+};
+console.log(quickSort([7, -17, 22, 11, 5, -3]));
+console.log(quickSort([10, 5, 2, 3, 0]));
+console.log(quickSort([-10, -55, -2, -3]));
